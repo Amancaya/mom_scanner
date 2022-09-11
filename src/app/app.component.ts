ced025de52a3e388729af8eb1c34fb5db768bd12
Amancaya <koda01996@gmail.com>
2022-09-11 15:07:33 -0400

    Firestore implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { InvoiceService } from './invoice.service';
+
+export interface IDocumentScanneer {
+  id: string,
+  text: Array<string>
+}
+
+export class DocumentScanneer implements IDocumentScanneer {
+  constructor(
+    public text: Array<string>,
+    public id: string
+   ) {
+    this.id = id
+    this.text = text
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'moms-scanner';
+  eventSubscriber: Subscription = new Subscription();
+  showLoader: Boolean = true;
+  documentts: IDocumentScanneer[] = [];
+
+  constructor(private service: InvoiceService) {}
+
+
+  ngOnInit(): void {
+    this.load()
+  }
+
+  ngOnDestroy() {
+    this.eventSubscriber.unsubscribe();
+  }
+
+  load() {
+    this.eventSubscriber.unsubscribe();
+    this.eventSubscriber = this.service.getAll().subscribe((result) => { 
+      this.documentts = result.map(rres => new DocumentScanneer(rres['text'], rres['id']))
+    })
+  }
+
+  deleteAll() {
+    this.service.deleteAll()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCM7iuSXT8LHpQqdYpuLVPdnvuebDnmv8Q",
+  authDomain: "momscanner-a3d4e.firebaseapp.com",
+  projectId: "momscanner-a3d4e",
+  storageBucket: "momscanner-a3d4e.appspot.com",
+  messagingSenderId: "639328760240",
+  appId: "1:639328760240:web:8b576142452b283d51e675"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    // provide modular style for AppCheck, see app.browser/server
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
